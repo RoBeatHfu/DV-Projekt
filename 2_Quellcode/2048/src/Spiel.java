366701b32df5b53f03f5ccfb43f14745193c5430
Eric1609 <eric.miller@hs-furtwangen.de>
2022-05-08 23:05:55 +0200

    Update Spiel.java

--- 2_Quellcode/2048/src/Spiel.java
+++ 2_Quellcode/2048/src/Spiel.java
@@ -3,8 +3,7 @@ public class Spiel {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        int spielfeld[][] = {{0,0,0,0},{0,0,0,0}};
-        System.out.println(spielfeld);
+        int[][] spielfeld = new int[4][4];
 	}
 
 }
