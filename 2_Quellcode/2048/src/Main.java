9b502183e92736b65e621dc5c6fd4756760e9a2e
Butscher <uic88659@contiwan.com>
2022-06-01 17:42:14 +0200

    Update Main.java

--- 2_Quellcode/2048/src/Main.java
+++ 2_Quellcode/2048/src/Main.java
@@ -1,9 +1,8 @@
 
 public class Main {
 
-		public static void main(String args[]) {
-			
-GUI_Menü g1m= new GUI_Menü();
-new GUI_Menü().setVisible(true);
+	public static void main(String args[]) {
+		GUI_Menue g1m= new GUI_Menue();
+		new GUI_Menue().setVisible(true);
 	}
 }
