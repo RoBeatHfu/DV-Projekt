297c23043b704005269d9d622436a1f90cd8cc37
Butscher <uic88659@contiwan.com>
2022-06-01 17:57:18 +0200

    Update Main.java

--- 2_Quellcode/2048/src/Main.java
+++ 2_Quellcode/2048/src/Main.java
@@ -2,7 +2,8 @@
 public class Main {
 
 	public static void main(String args[]) {
-		GUI_Menue g1m= new GUI_Menue();
-		new GUI_Menue().setVisible(true);
+		GUI_Menue g1m = new GUI_Menue();
+		g1m.setVisible(true);					//ist das nicht sinnvoller?
+		//new GUI_Menue().setVisible(true);
 	}
 }
