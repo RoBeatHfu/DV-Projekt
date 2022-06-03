9dfe7eb7921624800478d11dff10847e7969abae
adrian01m <adrian.tim.martin@hs-furtwangen.de>
2022-06-03 11:23:23 +0200

    Adrian

--- 2_Quellcode/2048/src/GUI_Spiel.java
+++ 2_Quellcode/2048/src/GUI_Spiel.java
@@ -236,13 +236,7 @@ public class GUI_Spiel extends javax.swing.JFrame {
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {   
     	setVisible(false);
-<<<<<<< HEAD
-    	new GUI_Menü().setVisible(true);
-  
-=======
-    	new GUI_Menue().setVisible(true);
-    	
->>>>>>> 84e029f3a9675785fc1537f81714b309f3339434
+        new GUI_Menue().setVisible(true);
     }                                        
 
   
