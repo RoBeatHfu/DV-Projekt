82f7f42a3bc1b7f860905955d6f0d543810cbafd
Butscher <102016437+Sumlesna@users.noreply.github.com>
2022-06-06 14:31:36 +0200

    Update Steuerung.java

--- 2_Quellcode/2048/src/Steuerung.java
+++ 2_Quellcode/2048/src/Steuerung.java
@@ -1,7 +1,7 @@
 
 public class Steuerung {
 
-	int [] felder = new int [16];							//Array contains the values of the fields
+	static int [] felder = new int [16];					//Array contains the values of the fields
 	
 	/**
 	 * chooses a random field and checks if it is free
@@ -20,27 +20,25 @@ public class Steuerung {
 	 * @param feldIndex
 	 * @return
 	 */
-	public static boolean fieldFree(int feldIndex) {
-		for(int i = 1; i < 17; i++) {
-			
+	public static boolean fieldFree(int fieldIndex) {
+		if(felder[fieldIndex] == 0) {
+			return true;
+		} else {
+			return false;
 		}
-		return true;										//To-Do
 	}
 	
 	/**returns the value of a field
 	 * @param feldIndex
 	 * @return
 	 */
-	public int getValue(int feldIndex) {
-		int wert = felder[feldIndex]; 						
+	public int getValue(int fieldIndex) {
+		int wert = felder[fieldIndex]; 						
 		return wert;
 	}
 	
 	
 	public static void main(String args[]) {				//Zum Testen während dem entwickeln
-		for(int i = 0; i<100; i++) {
-			int a = getIndexRandomFreeField();
-			System.out.println(a);
-		}
+		
 	}
 }
