b431aec933e1f5b8ffccd58db64be1cd2961f402
Butscher <102016437+Sumlesna@users.noreply.github.com>
2022-06-07 17:09:53 +0200

    Update Steuerung.java

--- 2_Quellcode/2048/src/Steuerung.java
+++ 2_Quellcode/2048/src/Steuerung.java
@@ -2,10 +2,22 @@
 public class Steuerung {
 
 	static int [] field = new int [16];					//Array contains the values of the fields
-	int score = 0;
+	static int score = 0;
+	
+	
 	
 	/**
-	 * chooses a random field and checks if it is free
+	 * fills a random free field with 2 or 4
+	 * use this method after any move
+	 * increases the score by one 
+	 */
+	public static void fillFieldAfterMove() {
+		field[getIndexRandomFreeField()] =  generateNr();
+		score++;
+	}
+	
+	/**
+	 * chooses a random free field
 	 * @return
 	 */
 	public static int getIndexRandomFreeField(){
@@ -29,7 +41,8 @@ public class Steuerung {
 		}
 	}
 	
-	/**returns the value of a field
+	/**
+	 * returns the value of a field
 	 * @param feldIndex
 	 * @return
 	 */
@@ -39,7 +52,7 @@ public class Steuerung {
 	}
 	
 	/**
-	 * returns 2 or 4 or the next move
+	 * returns 2 or 4 for the next move
 	 * @return
 	 */
 	public static int generateNr() {
@@ -50,16 +63,37 @@ public class Steuerung {
 			return 2;
 		}
 	}
-		
-	public static void main(String args[]) {				//Zum Testen während dem entwickeln
+	
+	
+	/**
+	 * assigns the value 0 to each field
+	 */
+	public static void clearFields() {
 		for(int i = 0; i <16; i++) {
 			field[i] = 0;
 		}
-		for(int i = 0; i <10; i++) {
-			field[getIndexRandomFreeField()] = generateNr();
+	}
+	
+	/**
+	 * gives the value of every field
+	 */
+	public static void printFields() {
+		for(int i = 0; i < 16; i = i + 4) {
+			System.out.println(field[i]+"\t"+field[i+1]+"\t"+field[i+2]+"\t"+field[i+3]);
 		}
-		for(int i = 0; i <16; i++) {
-			System.out.println(field[i]);
+	}
+		
+	public static void main(String args[]) {	
+		
+		//**************************************************//generate GUI
+		GUI_Menue g1m = new GUI_Menue();					
+		g1m.setVisible(true);
+		//**************************************************
+		
+		clearFields();
+		for(int i = 0; i< 10; i++) {
+			fillFieldAfterMove();
 		}
+		printFields();
 	}
 }
