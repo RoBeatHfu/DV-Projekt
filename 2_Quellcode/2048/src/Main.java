b0e3de8674100d8648c33067c37f7c6574d93146
Butscher <102016437+Sumlesna@users.noreply.github.com>
2022-06-07 17:40:04 +0200

    Update Main.java

--- 2_Quellcode/2048/src/Main.java
+++ 2_Quellcode/2048/src/Main.java
@@ -2,8 +2,12 @@
 public class Main {
 
 	public static void main(String args[]) {
+		//*********************************************//generate GUI
 		GUI_Menue g1m = new GUI_Menue();
-		g1m.setVisible(true);					//ist das nicht sinnvoller?
-		//new GUI_Menue().setVisible(true);
+		g1m.setVisible(true);
+		//*********************************************//generate Steuerung
+		Fields f1 = new Fields();
+		
+		
 	}
 }
