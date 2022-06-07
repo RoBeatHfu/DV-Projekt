5cd7fc26288b906a6dd23e61063bca271018c8f3
Sumlesna <dominik.butscher@hs-furtwangen.de>
2022-06-07 22:42:18 +0200

    Update Main.java

--- 2_Quellcode/2048/src/Main.java
+++ 2_Quellcode/2048/src/Main.java
@@ -1,17 +1,10 @@
-
 public class Main {
 
 	public static void main(String args[]) {
 		//*********************************************//generate GUI
-		//GUI_Menue g1m = new GUI_Menue();
-		//g1m.setVisible(true);
+		GUI_Menue g1 = new GUI_Menue();
+		g1.setVisible(true);
 		//*********************************************//generate Fields
 		Fields f1 = new Fields();
-		for(int i = 0; i < 10; i++) {
-			f1.fillFieldAfterMove();
-		}
-		f1.printFields();
-		f1.moveUp('t');
-		f1.printFields();	
 	}
 }
