1b0072f2891206ea63ebf69b540c9d86bc004090
sumlesna <dominik.butscher@hs-furtwangen.de>
2022-06-08 07:49:59 +0200

    Update Main.java

--- 2_Quellcode/2048/src/Main.java
+++ 2_Quellcode/2048/src/Main.java
@@ -6,5 +6,17 @@ public class Main {
 		g1.setVisible(true);
 		//*********************************************//generate Fields
 		Fields f1 = new Fields();
+		//*********************************************
+		
+		
+		
+		for(int i = 0; i < 8; i++) {
+			f1.fillFieldAfterMove();
+		}
+		
+		f1.printFields();
+		f1.fusion('b');
+		f1.fusion('l');
+		f1.getScore();
 	}
 }
