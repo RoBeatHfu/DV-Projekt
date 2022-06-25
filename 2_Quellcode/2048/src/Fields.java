0bbd236d359b21d372179926dbbfd9af1a0a489c
Sumlesna <dominik.butscher@hs-furtwangen.de>
2022-06-25 17:19:30 +0200

    Update Fields.java

--- 2_Quellcode/2048/src/Fields.java
+++ 2_Quellcode/2048/src/Fields.java
@@ -1,12 +1,12 @@
 
 public class Fields {
 
-	int [] field = new int [16];					//Array contains the values of the fields
-	int [] oldField = new int [16];					//to check if a move was possible
+	int[] field = new int[16]; // Array contains the values of the fields
+	int[] oldField = new int[16]; // to check if a move was possible
 	int score = 0;
 	int fusions = 0;
 	boolean hasMoved = false;
-	
+
 	public Fields() {
 		System.out.println("Fields");
 		clearFields();
@@ -16,126 +16,128 @@ public class Fields {
 		System.out.println();
 		printFields();
 	}
-	
+
 	/**
-	 * pushes fields to an edge and fusionate two fields with same values together in the opposite direction
-	 * after fusion, it is pushed towards the edge again. 
+	 * pushes fields to an edge and fusionate two fields with same values together
+	 * in the opposite direction after fusion, it is pushed towards the edge again.
 	 * Fills a random free field with 2 or 4 if something was movable or fusionable
+	 * 
 	 * @param direction
 	 */
 	public void fusion(char direction) {
 		hasMoved = false;
-		for(int i = 0; i < 16; i++) {
+		for (int i = 0; i < 16; i++) {
 			oldField[i] = field[i];
 		}
 		moveUp(direction);
 		switch (direction) {
 		case 't':
-			for(int k = 0; k < 4; k++) {
-				for(int i = 0+k; i < 12+k; i = i+4) {
-					if(isFusionable(i, i+4)) {
-						setValue(i, getValue(i)*2);
-						setValue(i+4, 0);						
+			for (int k = 0; k < 4; k++) {
+				for (int i = 0 + k; i < 12 + k; i = i + 4) {
+					if (isFusionable(i, i + 4)) {
+						setValue(i, getValue(i) * 2);
+						setValue(i + 4, 0);
 					}
 				}
 			}
 			break;
 		case 'b':
-			for(int k = 0; k < 4; k++) {
-				for(int i = 12+k; i > 0+k; i = i-4) {
-					if(isFusionable(i, i-4)) {
-						setValue(i, getValue(i)*2);
-						setValue(i-4, 0);													
+			for (int k = 0; k < 4; k++) {
+				for (int i = 12 + k; i > 0 + k; i = i - 4) {
+					if (isFusionable(i, i - 4)) {
+						setValue(i, getValue(i) * 2);
+						setValue(i - 4, 0);
 					}
 				}
 			}
 			break;
 		case 'l':
-			for(int k = 0; k < 15; k = k+4) {
-				for(int i = 0+k; i < 3+k; i++) {
-					if(isFusionable(i, i+1)) {
-						setValue(i, getValue(i)*2);
-						setValue(i+1, 0);
+			for (int k = 0; k < 15; k = k + 4) {
+				for (int i = 0 + k; i < 3 + k; i++) {
+					if (isFusionable(i, i + 1)) {
+						setValue(i, getValue(i) * 2);
+						setValue(i + 1, 0);
 					}
 				}
 			}
 			break;
 		case 'r':
-			for(int k = 0; k < 15; k = k+4) {
-				for(int i = 3+k; i > k; i--) {
-					if(isFusionable(i, i-1)) {
-						setValue(i, getValue(i)*2);
-						setValue(i-1, 0);
+			for (int k = 0; k < 15; k = k + 4) {
+				for (int i = 3 + k; i > k; i--) {
+					if (isFusionable(i, i - 1)) {
+						setValue(i, getValue(i) * 2);
+						setValue(i - 1, 0);
 					}
 				}
 			}
 			break;
 		}
 		moveUp(direction);
-		for(int i = 0; i < 16; i++) {
-			if(oldField[i] != field[i]) {
+		for (int i = 0; i < 16; i++) {
+			if (oldField[i] != field[i]) {
 				hasMoved = true;
 			}
 		}
-		if(hasMoved) {
+		if (hasMoved) {
 			fillFieldAfterMove();
 			printFields();
 		} else {
 			System.out.println("move not possible");
 		}
 	}
-		
+
 	/**
-	 * moves all fields to the edge in one direction 
-	 * and skips all zeroes in this direction
+	 * moves all fields to the edge in one direction and skips all zeroes in this
+	 * direction
+	 * 
 	 * @param direction 't' 'b' 'l' 'r'
 	 * @return whether it was possible to move
 	 */
 	private void moveUp(char direction) {
 		switch (direction) {
 		case 't':
-			for(int k = 0; k < 4; k++) {
-				for(int j = 0; j < 4; j++) {
-					for(int i = 0+k; i < 12+k; i = i+4) {
-						if(getValue(i) == 0) {
-							field[i] = field[i+4];
-							field[i+4] = 0;
+			for (int k = 0; k < 4; k++) {
+				for (int j = 0; j < 4; j++) {
+					for (int i = 0 + k; i < 12 + k; i = i + 4) {
+						if (getValue(i) == 0) {
+							field[i] = field[i + 4];
+							field[i + 4] = 0;
 						}
 					}
 				}
 			}
 			break;
 		case 'b':
-			for(int k = 0; k < 4; k++) {
-				for(int j = 0; j < 3; j++) {
-					for(int i = 12+k; i > 0+k; i = i-4) {
-						if(getValue(i) == 0) {
-							field[i] = field[i-4];
-							field[i-4] = 0;
+			for (int k = 0; k < 4; k++) {
+				for (int j = 0; j < 3; j++) {
+					for (int i = 12 + k; i > 0 + k; i = i - 4) {
+						if (getValue(i) == 0) {
+							field[i] = field[i - 4];
+							field[i - 4] = 0;
 						}
 					}
 				}
 			}
 			break;
 		case 'l':
-			for(int k = 0; k < 15; k = k+4) {
-				for(int j = 0; j < 3; j++) {
-					for(int i = 0+k; i < 3+k; i++) {
-						if(getValue(i) == 0 && i % 4 != 3) {
-							field[i] = field[i+1];
-							field[i+1] = 0;
+			for (int k = 0; k < 15; k = k + 4) {
+				for (int j = 0; j < 3; j++) {
+					for (int i = 0 + k; i < 3 + k; i++) {
+						if (getValue(i) == 0 && i % 4 != 3) {
+							field[i] = field[i + 1];
+							field[i + 1] = 0;
 						}
 					}
 				}
 			}
 			break;
 		case 'r':
-			for(int k = 0; k < 15; k = k+4) {
-				for(int j = 0; j < 3; j++) {
-					for(int i = 3+k; i > k; i--) {
-						if(getValue(i) == 0) {
-							field[i] = field[i-1];
-							field[i-1] = 0;
+			for (int k = 0; k < 15; k = k + 4) {
+				for (int j = 0; j < 3; j++) {
+					for (int i = 3 + k; i > k; i--) {
+						if (getValue(i) == 0) {
+							field[i] = field[i - 1];
+							field[i - 1] = 0;
 						}
 					}
 				}
@@ -143,51 +145,52 @@ public class Fields {
 			break;
 		}
 	}
-	
+
 	/**
 	 * checks whether two fields have the same value and are fusionable
+	 * 
 	 * @param x
 	 * @param y
 	 * @return
 	 */
 	private boolean isFusionable(int x, int y) {
-		if(getValue(x) == getValue(y) && isGameOver() == false ) {
+		if (getValue(x) == getValue(y)) {
 			return true;
-		
+
 		} else {
 			return false;
 		}
 	}
-	
+
 	/**
-	 * fills a random free field with 2 or 4
-	 * use this method after any move
-	 * increases the score by one 
+	 * fills a random free field with 2 or 4 use this method after any move
+	 * increases the score by one
 	 */
 	private void fillFieldAfterMove() {
-		field[getIndexRandomFreeField()] =  generateNr();
+		field[getIndexRandomFreeField()] = generateNr();
 	}
-	
-	
+
 	/**
 	 * chooses a random free field
+	 * 
 	 * @return index
 	 */
-	private int getIndexRandomFreeField(){
+	private int getIndexRandomFreeField() {
 		int z = 0;
 		do {
 			z = (int) (Math.random() * 20);
-		} while ( z < 0 || z > 15 || fieldFree(z) == false);
+		} while (z < 0 || z > 15 || fieldFree(z) == false);
 		return z;
 	}
-	
+
 	/**
 	 * checks whether a field is free
+	 * 
 	 * @param feldIndex
-	 * @return 
+	 * @return
 	 */
 	private boolean fieldFree(int fieldIndex) {
-		if(getValue(fieldIndex) == 0) {
+		if (getValue(fieldIndex) == 0) {
 			return true;
 		} else {
 			return false;
@@ -196,74 +199,110 @@ public class Fields {
 
 	/**
 	 * calculates the sum of all fields
+	 * 
 	 * @return score
 	 */
 	public int getScore() {
 		int score = 0;
-		for(int i = 0; i < 16; i++) {
+		for (int i = 0; i < 16; i++) {
 			score = score + getValue(i);
 		}
 		return score;
 	}
-	
+
 	/**
 	 * returns the value of a field
+	 * 
 	 * @param feldIndex
 	 * @return value
 	 */
 	public int getValue(int fieldIndex) {
-		int wert = field[fieldIndex]; 						
+		int wert = field[fieldIndex];
 		return wert;
 	}
-	
+
 	/**
 	 * sets a value into a field
+	 * 
 	 * @param fieldIndex
 	 * @param value
 	 */
 	private void setValue(int fieldIndex, int value) {
 		field[fieldIndex] = value;
 	}
-	
+
 	/**
 	 * chooses between 2 and 4
+	 * 
 	 * @return 2 or 4
 	 */
 	private int generateNr() {
-		int nr= (int) (Math.random()*10);
-		if(nr == 0) {
+		int nr = (int) (Math.random() * 10);
+		if (nr == 0) {
 			return 4;
 		} else {
 			return 2;
 		}
 	}
-	
+
 	/**
 	 * assigns the value 0 to each field
 	 */
 	private void clearFields() {
-		for(int i = 0; i <16; i++) {
+		for (int i = 0; i < 16; i++) {
 			field[i] = 0;
 		}
 	}
-	
+
 	/**
 	 * prints the value of every field in the console
 	 */
 	public void printFields() {
-		for(int i = 0; i < 16; i = i + 4) {
-			System.out.println(field[i]+"\t"+field[i+1]+"\t"+field[i+2]+"\t"+field[i+3]);
+		for (int i = 0; i < 16; i = i + 4) {
+			System.out.println(field[i] + "\t" + field[i + 1] + "\t" + field[i + 2] + "\t" + field[i + 3]);
 		}
 		System.out.println();
 	}
-	
+
+	/**
+	 * Checks whether the game is over by first searching for a free field.
+	 * If that was not successful, the method checks whether another move is possible.
+	 * @return
+	 */
 	public boolean isGameOver() {
-		boolean gameOver = true;
-		for(int i = 0; i < 16; i++) {
-			if(getValue(i) == 0) {
-				gameOver = false;
+		for (int i = 0; i < 16; i++) {				//field free?
+			if (getValue(i) == 0) {
+				return false;
 			}
 		}
-		return gameOver;
+		for (int k = 0; k < 4; k++) {				//possible fusion in direction 't'?
+			for (int i = 0 + k; i < 12 + k; i = i + 4) {
+				if (isFusionable(i, i + 4)) {
+					return false;
+				}
+			}
+		}
+		for (int k = 0; k < 4; k++) {				//possible fusion in direction 'b'?
+			for (int i = 12 + k; i > 0 + k; i = i - 4) {
+				if (isFusionable(i, i - 4)) {
+					return false;
+				}
+			}
+		}
+		for (int k = 0; k < 15; k = k + 4) {		//possible fusion in direction 'l'?
+			for (int i = 0 + k; i < 3 + k; i++) {
+				if (isFusionable(i, i + 1)) {
+					return false;
+				}
+			}
+		}
+		for (int k = 0; k < 15; k = k + 4) {		//possible fusion in direction 'r'?
+			for (int i = 3 + k; i > k; i--) {
+				if (isFusionable(i, i - 1)) {
+					return false;
+				}
+			}
+		}
+		return true;
 	}
 }
