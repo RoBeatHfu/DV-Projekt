5787ff5f23b6a21f9670ba6a0697ceaf2ce55aea
RoBeatHfu <robert.voss@hs-furtwangen.de>
2022-06-25 09:00:12 +0200

    Update GUI_Spiel.java
    Felder leer statt 0 und Struktur Verbesserung

--- 2_Quellcode/2048/src/GUI_Spiel.java
+++ 2_Quellcode/2048/src/GUI_Spiel.java
@@ -1,3 +1,4 @@
+
 /*******Adrian******/
 
 import java.awt.event.KeyEvent;
@@ -27,7 +28,7 @@ public class GUI_Spiel extends javax.swing.JFrame implements KeyListener {
 	Fields fieldSpiel;
 	int punkte;
 	int feld1;
-	
+
 	public GUI_Spiel() {
 
 		// refresh des Spielfelds nach jedem Zug!!!
@@ -36,8 +37,8 @@ public class GUI_Spiel extends javax.swing.JFrame implements KeyListener {
 		addKeyListener(this);
 		setFocusable(true);
 		setFocusTraversalKeysEnabled(false);
-		//punkte = fieldSpiel.getScore(); 
-		//feld1 = fieldSpiel.getValue(0);
+		// punkte = fieldSpiel.getScore();
+		// feld1 = fieldSpiel.getValue(0);
 	}
 
 	// Überschreibt die Tastatureingaben Verarbeitung
@@ -60,27 +61,43 @@ public class GUI_Spiel extends javax.swing.JFrame implements KeyListener {
 			fieldSpiel.fusion('t');
 		}
 		System.out.println();
-		for(int i = 0; i < 16; i++) {
+		for (int i = 0; i < 16; i++) {
 			setValue(i, fieldSpiel.getValue(i));
 		}
 		setValue(16, fieldSpiel.getScore());
 		fieldSpiel.getScore();
-		jLabel1.setBackground((new java.awt.Color(setColorR(fieldSpiel.getValue(0)), setColorG(fieldSpiel.getValue(0)), setColorB(fieldSpiel.getValue(0)))));
-		jLabel2.setBackground((new java.awt.Color(setColorR(fieldSpiel.getValue(1)), setColorG(fieldSpiel.getValue(1)), setColorB(fieldSpiel.getValue(1)))));
-		jLabel3.setBackground((new java.awt.Color(setColorR(fieldSpiel.getValue(2)), setColorG(fieldSpiel.getValue(2)), setColorB(fieldSpiel.getValue(2)))));
-		jLabel4.setBackground((new java.awt.Color(setColorR(fieldSpiel.getValue(3)), setColorG(fieldSpiel.getValue(3)), setColorB(fieldSpiel.getValue(3)))));
-		jLabel5.setBackground((new java.awt.Color(setColorR(fieldSpiel.getValue(4)), setColorG(fieldSpiel.getValue(4)), setColorB(fieldSpiel.getValue(4)))));
-		jLabel6.setBackground((new java.awt.Color(setColorR(fieldSpiel.getValue(5)), setColorG(fieldSpiel.getValue(5)), setColorB(fieldSpiel.getValue(5)))));
-		jLabel7.setBackground((new java.awt.Color(setColorR(fieldSpiel.getValue(6)), setColorG(fieldSpiel.getValue(6)), setColorB(fieldSpiel.getValue(6)))));
-		jLabel8.setBackground((new java.awt.Color(setColorR(fieldSpiel.getValue(7)), setColorG(fieldSpiel.getValue(7)), setColorB(fieldSpiel.getValue(7)))));
-		jLabel9.setBackground((new java.awt.Color(setColorR(fieldSpiel.getValue(8)), setColorG(fieldSpiel.getValue(8)), setColorB(fieldSpiel.getValue(8)))));
-		jLabel10.setBackground((new java.awt.Color(setColorR(fieldSpiel.getValue(9)), setColorG(fieldSpiel.getValue(9)), setColorB(fieldSpiel.getValue(9)))));
-		jLabel11.setBackground((new java.awt.Color(setColorR(fieldSpiel.getValue(10)), setColorG(fieldSpiel.getValue(10)), setColorB(fieldSpiel.getValue(10)))));
-		jLabel12.setBackground((new java.awt.Color(setColorR(fieldSpiel.getValue(11)), setColorG(fieldSpiel.getValue(11)), setColorB(fieldSpiel.getValue(11)))));
-		jLabel13.setBackground((new java.awt.Color(setColorR(fieldSpiel.getValue(12)), setColorG(fieldSpiel.getValue(12)), setColorB(fieldSpiel.getValue(12)))));
-		jLabel14.setBackground((new java.awt.Color(setColorR(fieldSpiel.getValue(13)), setColorG(fieldSpiel.getValue(13)), setColorB(fieldSpiel.getValue(13)))));
-		jLabel15.setBackground((new java.awt.Color(setColorR(fieldSpiel.getValue(14)), setColorG(fieldSpiel.getValue(14)), setColorB(fieldSpiel.getValue(14)))));
-		jLabel16.setBackground((new java.awt.Color(setColorR(fieldSpiel.getValue(15)), setColorG(fieldSpiel.getValue(15)), setColorB(fieldSpiel.getValue(15)))));
+		jLabel1.setBackground((new java.awt.Color(setColorR(fieldSpiel.getValue(0)), setColorG(fieldSpiel.getValue(0)),
+				setColorB(fieldSpiel.getValue(0)))));
+		jLabel2.setBackground((new java.awt.Color(setColorR(fieldSpiel.getValue(1)), setColorG(fieldSpiel.getValue(1)),
+				setColorB(fieldSpiel.getValue(1)))));
+		jLabel3.setBackground((new java.awt.Color(setColorR(fieldSpiel.getValue(2)), setColorG(fieldSpiel.getValue(2)),
+				setColorB(fieldSpiel.getValue(2)))));
+		jLabel4.setBackground((new java.awt.Color(setColorR(fieldSpiel.getValue(3)), setColorG(fieldSpiel.getValue(3)),
+				setColorB(fieldSpiel.getValue(3)))));
+		jLabel5.setBackground((new java.awt.Color(setColorR(fieldSpiel.getValue(4)), setColorG(fieldSpiel.getValue(4)),
+				setColorB(fieldSpiel.getValue(4)))));
+		jLabel6.setBackground((new java.awt.Color(setColorR(fieldSpiel.getValue(5)), setColorG(fieldSpiel.getValue(5)),
+				setColorB(fieldSpiel.getValue(5)))));
+		jLabel7.setBackground((new java.awt.Color(setColorR(fieldSpiel.getValue(6)), setColorG(fieldSpiel.getValue(6)),
+				setColorB(fieldSpiel.getValue(6)))));
+		jLabel8.setBackground((new java.awt.Color(setColorR(fieldSpiel.getValue(7)), setColorG(fieldSpiel.getValue(7)),
+				setColorB(fieldSpiel.getValue(7)))));
+		jLabel9.setBackground((new java.awt.Color(setColorR(fieldSpiel.getValue(8)), setColorG(fieldSpiel.getValue(8)),
+				setColorB(fieldSpiel.getValue(8)))));
+		jLabel10.setBackground((new java.awt.Color(setColorR(fieldSpiel.getValue(9)), setColorG(fieldSpiel.getValue(9)),
+				setColorB(fieldSpiel.getValue(9)))));
+		jLabel11.setBackground((new java.awt.Color(setColorR(fieldSpiel.getValue(10)),
+				setColorG(fieldSpiel.getValue(10)), setColorB(fieldSpiel.getValue(10)))));
+		jLabel12.setBackground((new java.awt.Color(setColorR(fieldSpiel.getValue(11)),
+				setColorG(fieldSpiel.getValue(11)), setColorB(fieldSpiel.getValue(11)))));
+		jLabel13.setBackground((new java.awt.Color(setColorR(fieldSpiel.getValue(12)),
+				setColorG(fieldSpiel.getValue(12)), setColorB(fieldSpiel.getValue(12)))));
+		jLabel14.setBackground((new java.awt.Color(setColorR(fieldSpiel.getValue(13)),
+				setColorG(fieldSpiel.getValue(13)), setColorB(fieldSpiel.getValue(13)))));
+		jLabel15.setBackground((new java.awt.Color(setColorR(fieldSpiel.getValue(14)),
+				setColorG(fieldSpiel.getValue(14)), setColorB(fieldSpiel.getValue(14)))));
+		jLabel16.setBackground((new java.awt.Color(setColorR(fieldSpiel.getValue(15)),
+				setColorG(fieldSpiel.getValue(15)), setColorB(fieldSpiel.getValue(15)))));
 	}
 
 	// Die Methode muss auch aufgeführt sein
@@ -140,118 +157,118 @@ public class GUI_Spiel extends javax.swing.JFrame implements KeyListener {
 		jLabel1.setBackground(new java.awt.Color(153, 153, 153));
 		jLabel1.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 36));
 		jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-		//jLabel1.setText("0");
-		jLabel1.setText(""+feld1);
+		 jLabel1.setText("");
+		//jLabel1.setText("" + feld1);
 		jLabel1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
 
 		jLabel2.setOpaque(true);
 		jLabel2.setBackground(new java.awt.Color(153, 153, 153));
 		jLabel2.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 36));
 		jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-		jLabel2.setText("0");
+		jLabel2.setText("");
 		jLabel2.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
 
 		jLabel3.setOpaque(true);
 		jLabel3.setBackground(new java.awt.Color(153, 153, 153));
 		jLabel3.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 36));
 		jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-		jLabel3.setText("0");
+		jLabel3.setText("");
 		jLabel3.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
 
 		jLabel4.setOpaque(true);
 		jLabel4.setBackground(new java.awt.Color(153, 153, 153));
 		jLabel4.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 36));
 		jLabel4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-		jLabel4.setText("0");
+		jLabel4.setText("");
 		jLabel4.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
 
 		jLabel5.setOpaque(true);
 		jLabel5.setBackground(new java.awt.Color(153, 153, 153));
 		jLabel5.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 36));
 		jLabel5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-		jLabel5.setText("0");
+		jLabel5.setText("");
 		jLabel5.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
 
 		jLabel6.setOpaque(true);
 		jLabel6.setBackground(new java.awt.Color(153, 153, 153));
 		jLabel6.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 36));
 		jLabel6.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-		jLabel6.setText("0");
+		jLabel6.setText("");
 		jLabel6.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
 
 		jLabel7.setOpaque(true);
 		jLabel7.setBackground(new java.awt.Color(153, 153, 153));
 		jLabel7.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 36));
 		jLabel7.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-		jLabel7.setText("0");
+		jLabel7.setText("");
 		jLabel7.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
 
 		jLabel8.setOpaque(true);
 		jLabel8.setBackground(new java.awt.Color(153, 153, 153));
 		jLabel8.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 36));
 		jLabel8.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-		jLabel8.setText("0");
+		jLabel8.setText("");
 		jLabel8.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
 
 		jLabel9.setOpaque(true);
 		jLabel9.setBackground(new java.awt.Color(153, 153, 153));
 		jLabel9.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 36));
 		jLabel9.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-		jLabel9.setText("0");
+		jLabel9.setText("");
 		jLabel9.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
 
 		jLabel10.setOpaque(true);
 		jLabel10.setBackground(new java.awt.Color(153, 153, 153));
 		jLabel10.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 36));
 		jLabel10.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-		jLabel10.setText("0");
+		jLabel10.setText("");
 		jLabel10.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
 
 		jLabel11.setOpaque(true);
 		jLabel11.setBackground(new java.awt.Color(153, 153, 153));
 		jLabel11.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 36));
 		jLabel11.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-		jLabel11.setText("0");
+		jLabel11.setText("");
 		jLabel11.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
 
 		jLabel12.setOpaque(true);
 		jLabel12.setBackground(new java.awt.Color(153, 153, 153));
 		jLabel12.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 36));
 		jLabel12.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-		jLabel12.setText("0");
+		jLabel12.setText("");
 		jLabel12.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
 
 		jLabel13.setOpaque(true);
 		jLabel13.setBackground(new java.awt.Color(153, 153, 153));
 		jLabel13.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 36));
 		jLabel13.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-		jLabel13.setText("0");
+		jLabel13.setText("");
 		jLabel13.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
 
 		jLabel14.setOpaque(true);
 		jLabel14.setBackground(new java.awt.Color(153, 153, 153));
 		jLabel14.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 36));
 		jLabel14.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-		jLabel14.setText("0");
+		jLabel14.setText("");
 		jLabel14.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
 
 		jLabel15.setOpaque(true);
 		jLabel15.setBackground(new java.awt.Color(153, 153, 153));
 		jLabel15.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 36));
 		jLabel15.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-		jLabel15.setText("0");
+		jLabel15.setText("");
 		jLabel15.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
 
 		jLabel16.setOpaque(true);
 		jLabel16.setBackground(new java.awt.Color(153, 153, 153));
 		jLabel16.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 36));
 		jLabel16.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-		jLabel16.setText("0");
+		jLabel16.setText("");
 		jLabel16.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
 
 		jLabelScore.setFont(new java.awt.Font("Arial Rounded MT Bold", 1, 18));
 		jLabelScore.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-		jLabelScore.setText(""+punkte);
+		jLabelScore.setText("" + punkte);
 		jLabelScore.setToolTipText("");
 		jLabelScore.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "SCORE",
 				javax.swing.border.TitledBorder.CENTER, javax.swing.border.TitledBorder.TOP,
@@ -410,210 +427,163 @@ public class GUI_Spiel extends javax.swing.JFrame implements KeyListener {
 
 	/**
 	 * puts the value into the label of the gui
+	 * 
 	 * @param index
 	 * @param value
 	 */
 	private void setValue(int index, int value) {
-		switch (index) {
-		case 0:
-			jLabel1.setText("" + value);
-			break;
-		case 1:
-			jLabel2.setText("" + value);
-			break;
-		case 2:
-			jLabel3.setText("" + value);
-			break;
-		case 3:
-			jLabel4.setText("" + value);
-			break;
-		case 4:
-			jLabel5.setText("" + value);
-			break;
-		case 5:
-			jLabel6.setText("" + value);
-			break;
-		case 6:
-			jLabel7.setText("" + value);
-			break;
-		case 7:
-			jLabel8.setText("" + value);
-			break;
-		case 8:
-			jLabel9.setText("" + value);
-			break;
-		case 9:
-			jLabel10.setText("" + value);
-			break;
-		case 10:
-			jLabel11.setText("" + value);
-			break;
-		case 11:
-			jLabel12.setText("" + value);
-			break;
-		case 12:
-			jLabel13.setText("" + value);
-			break;
-		case 13:
-			jLabel14.setText("" + value);
-			break;
-		case 14:
-			jLabel15.setText("" + value);
-			break;
-		case 15:
-			jLabel16.setText("" + value);
-			break;
-		case 16:
-			jLabelScore.setText(""+value);
-			break;
-		default:
-			System.out.println("invalid index for fieldsSpiel");
-		}
+		javax.swing.JLabel[] arrObj = { jLabel1, jLabel2, jLabel3, jLabel4, jLabel5, jLabel6, jLabel7, jLabel8, jLabel9,
+				jLabel10, jLabel11, jLabel12, jLabel13, jLabel14, jLabel15, jLabel16, jLabelScore };
+		if (value == 0)
+			arrObj[index].setText("");
+		else
+			arrObj[index].setText("" + value);
+
 	}
-	
+
 	private int setColorR(int value) {
 		int color = 0;
-		switch(value) {
-		case 0:
-			color = 155;
-			break;
-		case 2:
-			color = 138;
-			break;
-		case 4:
-			color = 120;
-			break;
-		case 8:
-			color = 101;
-			break;
-		case 16:
-			color = 83;
-			break;
-		case 32:
-			color = 68;
-			break;
-		case 64:
-			color = 55;
-			break;
-		case 128:
-			color = 42;
-			break;
-		case 256:
-			color = 31;
-			break;
-		case 512:
-			color = 22;
-			break;
-		case 1024:
-			color = 13;
-			break;
-		case 2048:
-			color = 6;
-			break;
-		case 4098:
-			color = 0;
-			break;
-		default:
-			System.out.println("sorry, you are too good we have no more colors ;)");
-			break;
+		switch (value) {
+			case 0:
+				color = 155;
+				break;
+			case 2:
+				color = 138;
+				break;
+			case 4:
+				color = 120;
+				break;
+			case 8:
+				color = 101;
+				break;
+			case 16:
+				color = 83;
+				break;
+			case 32:
+				color = 68;
+				break;
+			case 64:
+				color = 55;
+				break;
+			case 128:
+				color = 42;
+				break;
+			case 256:
+				color = 31;
+				break;
+			case 512:
+				color = 22;
+				break;
+			case 1024:
+				color = 13;
+				break;
+			case 2048:
+				color = 6;
+				break;
+			case 4098:
+				color = 0;
+				break;
+			default:
+				System.out.println("sorry, you are too good we have no more colors ;)");
+				break;
 		}
 		return color;
 	}
-	
+
 	private int setColorG(int value) {
 		int color = 0;
-		switch(value) {
-		case 0:
-			color = 155;
-			break;
-		case 2:
-			color = 156;
-			break;
-		case 4:
-			color = 159;
-			break;
-		case 8:
-			color = 163;
-			break;
-		case 16:
-			color = 166;
-			break;
-		case 32:
-			color = 165;
-			break;
-		case 64:
-			color = 163;
-			break;
-		case 128:
-			color = 160;
-			break;
-		case 256:
-			color = 156;
-			break;
-		case 512:
-			color = 151;
-			break;
-		case 1024:
-			color = 144;
-			break;
-		case 2048:
-			color = 135;
-			break;
-		case 4098:
-			color = 126;
-			break;
-		default:
-			System.out.println("sorry, you are too good we have no more colors ;)");
-			break;
+		switch (value) {
+			case 0:
+				color = 155;
+				break;
+			case 2:
+				color = 156;
+				break;
+			case 4:
+				color = 159;
+				break;
+			case 8:
+				color = 163;
+				break;
+			case 16:
+				color = 166;
+				break;
+			case 32:
+				color = 165;
+				break;
+			case 64:
+				color = 163;
+				break;
+			case 128:
+				color = 160;
+				break;
+			case 256:
+				color = 156;
+				break;
+			case 512:
+				color = 151;
+				break;
+			case 1024:
+				color = 144;
+				break;
+			case 2048:
+				color = 135;
+				break;
+			case 4098:
+				color = 126;
+				break;
+			default:
+				System.out.println("sorry, you are too good we have no more colors ;)");
+				break;
 		}
 		return color;
 	}
-	
+
 	private int setColorB(int value) {
 		int color = 0;
-		switch(value) {
-		case 0:
-			color = 155;
-			break;
-		case 2:
-			color = 150;
-			break;
-		case 4:
-			color = 145;
-			break;
-		case 8:
-			color = 141;
-			break;
-		case 16:
-			color = 137;
-			break;
-		case 32:
-			color = 131;
-			break;
-		case 64:
-			color = 125;
-			break;
-		case 128:
-			color = 119;
-			break;
-		case 256:
-			color = 112;
-			break;
-		case 512:
-			color = 105;
-			break;
-		case 1024:
-			color = 98;
-			break;
-		case 2048:
-			color = 90;
-			break;
-		case 4098:
-			color = 82;
-			break;
-		default:
-			System.out.println("sorry, you are too good we have no more colors ;)");
-			break;
+		switch (value) {
+			case 0:
+				color = 155;
+				break;
+			case 2:
+				color = 150;
+				break;
+			case 4:
+				color = 145;
+				break;
+			case 8:
+				color = 141;
+				break;
+			case 16:
+				color = 137;
+				break;
+			case 32:
+				color = 131;
+				break;
+			case 64:
+				color = 125;
+				break;
+			case 128:
+				color = 119;
+				break;
+			case 256:
+				color = 112;
+				break;
+			case 512:
+				color = 105;
+				break;
+			case 1024:
+				color = 98;
+				break;
+			case 2048:
+				color = 90;
+				break;
+			case 4098:
+				color = 82;
+				break;
+			default:
+				System.out.println("sorry, you are too good we have no more colors ;)");
+				break;
 		}
 		return color;
 	}
