7486d6bd853d097db6b4c4df6d43778055e53103
NickRothmund <101902532+NickRothmund@users.noreply.github.com>
2022-06-27 10:40:09 +0200

    Update of GitHub Connection

--- 2_Quellcode/2048/GitHubDownload.py
+++ 2_Quellcode/2048/GitHubDownload.py
@@ -2,7 +2,7 @@ from msilib.schema import File
 from github import Github
 import os
 
-g = Github("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")      #connect to github via acces token
+g = Github("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")      #connect to github via acces token
 repo = g.get_repo("RoBeatHfu/DV-Projekt")                   #get our project repository as object
 directory = os.path.dirname(os.path.abspath(__file__))      #get path to directory so that it can find it even when users save it in a different directory
 filename = os.path.join(directory, "Highscore.md")          #make it an absolute path to a file instead of a directory
---
+++ 2_Quellcode/2048/GitHubUpload.py
@@ -0,0 +1,17 @@
+from msilib.schema import File
+from github import Github
+import os
+
+g = Github("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")      #connect to github via acces token
+repo = g.get_repo("RoBeatHfu/DV-Projekt")                   #get our project repository as object
+directory = os.path.dirname(os.path.abspath(__file__))      #get path to file so that it can find it even when users save it in a different directory
+filename = os.path.join(directory, "Highscore.md")          #make it an absolute path to a file instead of a directory
+file = repo.get_contents("/2_Quellcode/2048/Highscore.md")                    #get content of the highscore, important as we need the path for the file we want to update
+highscoreFile = open(filename, "r")
+highscoreList = highscoreFile.readlines()
+highscoreFile.close()
+highscore = ""
+for player in highscoreList:
+    highscore = highscore + player
+print(highscore)
+repo.update_file(file.path, "update of Highscore", highscore, file.sha)
